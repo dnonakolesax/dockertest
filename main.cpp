8481075b6e93b45d34f3752f85d5b26ca2723063
George Bezrukov <domnakolesaxxx@gmail.com>
2023-04-29 21:29:15 +0000

    dbtry

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 // Copyright 2023 YOURMOM
 #include <iostream>
 #include "sample.hpp"
+#include <pqxx/pqxx>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[]) {\
+    pqxx::connection conn("dbname=testdocker user=habrpguser password=pgpwd4habr \
+                   	hostaddr=172.19.0.2 port=5432");
     std::cout << "Hello, world!" << std::endl;
-    std::cout << evenOdd (11) << std::endl;
+  //  std::cout << evenOdd (11) << std::endl;
     return 0;
 }
