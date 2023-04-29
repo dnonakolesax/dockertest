a8e2b4947ae15b75de90ff7425c714c33e2fc048
George Bezrukov <domnakolesaxxx@gmail.com>
2023-04-29 21:46:22 +0000

    dbtry

--- main.cpp
+++ main.cpp
@@ -3,10 +3,23 @@
 #include "sample.hpp"
 #include <pqxx/pqxx>
 
-int main(int argc, char* argv[]) {\
+int main(int argc, char* argv[]) {
     pqxx::connection conn("dbname=testdocker user=habrpguser password=pgpwd4habr \
                    	hostaddr=172.19.0.2 port=5432");
-    std::cout << "Hello, world!" << std::endl;
+                    
+    pqxx::work txn(conn);
+
+    std::string sql = "SELECT * FROM testdocker.example";
+    pqxx::result r = txn.exec(sql);
+
+    txn.commit();
+
+    for (auto const &row: r) {
+        for (auto const &field: row) 
+            std::cout << field.c_str() << '\t';
+        std::cout << '\n';
+    }
+
   //  std::cout << evenOdd (11) << std::endl;
     return 0;
 }
