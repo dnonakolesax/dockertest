b7a99b81bc2a7b31de6f386210de23d083aa13c6
George Bezrukov <domnakolesaxxx@gmail.com>
2023-04-30 20:12:10 +0000

    ci

--- test/sample_test.cpp
+++ test/sample_test.cpp
@@ -5,8 +5,7 @@
 
 TEST (sample_test, sample_test) {
     EXPECT_EQ(tryDB(), true);
-    //connectDB();
-    std::cout << "complete" << std::endl;
+    connectDB();
 }
 
 int main(int argc, char** argv) {
