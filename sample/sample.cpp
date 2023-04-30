785631a45ae4297f7833fbef767deaa73a18ddd5
George Bezrukov <domnakolesaxxx@gmail.com>
2023-04-30 16:02:11 +0000

    dbci

--- sample/sample.cpp
+++ sample/sample.cpp
@@ -1,31 +1,24 @@
 #include "sample.hpp"
+#include <iostream>
 #include <pqxx/pqxx>
 
 bool tryDB () {
     try {
     pqxx::connection conn("dbname=testdocker user=habrpguser password=pgpwd4habr \
-                   	hostaddr=localhost port=5432");
+                    	hostaddr=127.0.0.1 port=5432");
 
     pqxx::work txn(conn);
-
-    if (!conn.is_open()) {
-        return false;
-    }
 
     std::string sql = "SELECT * FROM testdocker.example";
     pqxx::result r = txn.exec(sql);
 
     txn.commit();
+    conn.close();
 
-    // for (auto const &row: r) {
-    //     for (auto const &field: row) 
-    //         std::cout << field.c_str() << '\t';
-    //     std::cout << '\n';
-    // }
-
-    conn.close();}
-    catch (const pqxx::pqxx_exception &e) {
-        std::cerr << e.base().what() << std::endl;
+    std::cout << "uzbek" << std::endl;
+    }
+    catch (std::exception const &e) {
+        std::cerr << e.what() << std::endl;
     }
     return true;
 }
